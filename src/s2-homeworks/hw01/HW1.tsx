ecb1abf1da043bd0f2c6c3af41a58b34b03a5c2e
Kostushka <nastyatery47@yandex.ru>
2022-11-08 20:30:03 +0300

    Added hw01

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -1,44 +1,53 @@
-import React from 'react'
-import Message from './message/Message'
-import MessageSender from './message-sender/MessageSender'
-import s2 from '../../s1-main/App.module.css'
-import FriendMessage from './friend-message/FriendMessage'
-import avatar from './avatar.png'
+import React from 'react';
+import Message from './message/Message';
+import MessageSender from './message-sender/MessageSender';
+import s2 from '../../s1-main/App.module.css';
+import FriendMessage from './friend-message/FriendMessage';
+import avatar from './avatar.png';
 
 /*
-* 1 - описать тип MessageType
-* 2 - описать тип MessagePropsType в файле Message.tsx
-* 3 - в файле Message.tsx отобразить приходящие данные
-* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
-* 5 - сделать стили в соответствии с дизайном
-* */
+ * 5 - сделать стили в соответствии с дизайном
+ * */
 
-// нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+    id: number;
+    user: UserInMessageType;
+    message: MessageInMessageType;
+};
+
+type UserInMessageType = {
+    avatar: string;
+    name: string;
+};
+
+type MessageInMessageType = {
+    text: string;
+    time: string;
+};
 
 // структуру объекта не менять
 export const message0: MessageType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        name: 'John', // можно менять
     },
     message: {
         text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
         time: '22:00', // можно менять
     },
-}
+};
 export const friendMessage0: MessageType = {
     id: 100,
     user: {
         avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
+        name: 'Alexandr', // можно менять
     },
     message: {
         text: 'зеркальное сообщение для тренировки css', // можно менять
         time: '22:00', // можно менять
     },
-}
+};
 
 const HW1 = () => {
     return (
@@ -55,7 +64,7 @@ const HW1 = () => {
                 <MessageSender M={Message} />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default HW1
+export default HW1;
